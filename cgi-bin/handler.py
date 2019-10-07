4265620bc197ab9e44fe6b24f22b3806893ad5a4
Christoph Coijanovic <mail@coijanovic.com>
2019-10-07 16:51:24 +0200

    improved transaction formatting

--- cgi-bin/handler.py
+++ cgi-bin/handler.py
@@ -10,16 +10,27 @@ transrep = form.getvalue("transrep")
 fromacc = form.getvalue("fromacc")
 fromamount = form.getvalue("fromamount")
 toacc = form.getvalue("toacc")
+toamount = form.getvalue("toamount")
 
 if transclear == "on":
     c = " * "
 else:
     c = " "
 
+if fromamount:
+    fromamount = str(fromamount) + " EUR"
+else:
+    fromamount = ""
+
+if toamount:
+    toamount = str(toamount) + " EUR"
+else:
+    toamount = ""
+
 transdate = transdate.replace("-","/")
 
 ledgerfile = open("test.txt","a")
 ledgerfile.write(transdate + c + transrep + "\n")
-ledgerfile.write("    " + fromacc + "          " + fromamount + " EUR\n")
-ledgerfile.write("    " + toacc + "\n\n")
+ledgerfile.write("    " + fromacc + "          " + fromamount + "\n")
+ledgerfile.write("    " + toacc + "        " + toamount + "\n\n")
 ledgerfile.close()
